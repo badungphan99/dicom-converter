79352c965c316930a9a6e389be3a213ded802f86
Kim Cuc Nguyen <nguyenkimcuc299@gmail.com>
2019-12-25 23:46:19 +0700

    Open, display image

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QFileDialog>
+#include <QString>
+#include <QImage>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -13,3 +15,16 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_actionSelect_Image_triggered()
+{
+    const QString folderPath = QFileDialog::getOpenFileName(this, "Select file", "Images(*.png, *.jpg)");
+
+    if(!folderPath.isEmpty()){
+        int w = ui->label_pic->width();
+        int h = ui->label_pic->height();
+
+        QImage image(folderPath);
+        ui->label_pic->setPixmap(QPixmap::fromImage(image.scaled(w, h, Qt::KeepAspectRatio)));
+    }
+}
