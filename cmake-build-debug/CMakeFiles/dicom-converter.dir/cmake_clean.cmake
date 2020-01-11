bf11a9ef605b23ec919d1de2d000cc1413b7d25e
dungpb <badungphan99@gmail.com>
2020-01-11 22:47:40 +0700

    sau loi giao dien trong fuke ui_mainwindows.h

---
+++ cmake-build-debug/CMakeFiles/dicom-converter.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dicom-converter_autogen.dir/AutogenOldSettings.txt"
+  "CMakeFiles/dicom-converter_autogen.dir/ParseCache.txt"
+  "dicom-converter_autogen"
+  "CMakeFiles/dicom-converter.dir/dicom-converter_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/dicom-converter.dir/main.cpp.o"
+  "CMakeFiles/dicom-converter.dir/mainwindow.cpp.o"
+  "CMakeFiles/dicom-converter.dir/src/dicom.cpp.o"
+  "CMakeFiles/dicom-converter.dir/src/dicom_extract.cpp.o"
+  "CMakeFiles/dicom-converter.dir/src/system_manager.cpp.o"
+  "dicom-converter"
+  "dicom-converter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dicom-converter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
